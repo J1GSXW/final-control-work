af0442460414cea94c03102e7c7b66ed74009f53
Igor Andrushenko <igorandrushenko2001@gmail.com>
2023-04-28 14:10:18 +1000

    написал метод для создания массива из строк

--- Program.cs
+++ Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string[] CreateStringArray()
+{
+    Console.WriteLine("Введите количество элементов в массиве: ");
+    int size = Convert.ToInt32(Console.ReadLine());
+    string[] stringArray = new string[size];
+    int i = 0;
+    while(i < size)
+    {
+        Console.WriteLine($"Введите {i + 1} элемент: ");
+        string? element = Console.ReadLine()??String.Empty;
+        stringArray[i] = element;
+        i++;
+    }
+    return stringArray;
+}
+
+
+
