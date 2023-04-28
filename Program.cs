cd586b0bbe02aceeeaf8c6650c27f351b628a7c3
Igor Andrushenko <igorandrushenko2001@gmail.com>
2023-04-28 14:13:56 +1000

    написал метод для показа массивов и дописал программу

--- Program.cs
+++ Program.cs
@@ -28,4 +28,16 @@ string[] LessThenThreeSymbols(string[] array)
     return newArray;
 }
 
+void ShowArray(string[] array)
+{
+    for(int i = 0; i < array.Length; i++)
+        Console.Write(array[i] + " ");
+    Console.WriteLine();
+}
 
+
+string[] myArray = CreateStringArray();
+string[] myArray2 = LessThenThreeSymbols(myArray);
+ShowArray(myArray);
+Console.WriteLine("");
+ShowArray(myArray2);
